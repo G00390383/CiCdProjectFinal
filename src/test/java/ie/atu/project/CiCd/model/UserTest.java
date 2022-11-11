22c3d56a7d9f2f01ad82f755de85e7dfd14a9b28
G00398070 <G00398070@atu.ie>
2022-11-11 16:32:10 +0000

    Created Test class for user.

---
+++ src/test/java/ie/atu/project/CiCd/model/UserTest.java
@@ -0,0 +1,29 @@
+package ie.atu.project.CiCd.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void set_id_test_success()
+    {
+
+    }
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+
+}
